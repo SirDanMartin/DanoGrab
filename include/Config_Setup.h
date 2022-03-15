9580f372d92275488ffdb1c4f70215c13563fb12
SirDanMartin <59475245+SirDanMartin@users.noreply.github.com>
2022-03-15 19:58:49 +0000

    Pre-LCD Update

--- include/Config_Setup.h
+++ include/Config_Setup.h
@@ -5,13 +5,13 @@
 */
 
 //Movement Speeds (max speed 500, lower is faster)
-#define X_Speed                             800
-#define Y_Speed                             1000
-#define Z_Speed                             1000
-#define Default_Speed                       1000
-#define XY_Speed                            1000
-#define XY_Home_Speed                       1000
-#define Z_Home_Speed                        1000
+#define X_Speed                             300
+#define Y_Speed                             300
+#define Z_Speed                             300
+#define Default_Speed                       300
+#define XY_Speed                            300
+#define XY_Home_Speed                       300
+#define Z_Home_Speed                        300
 
 //Motor Direction - Changing the value to 1 to reverses motor directions
 #define X_Direction                         0
@@ -26,5 +26,5 @@
 -----------------
 */
 
-int G_Start_Time = 10;
+int G_Start_Time = 15;
 int Crane_Drop_Distance = 5; //Maximum distance to drop the crane. (Used if the limit switch does not trigger.)
